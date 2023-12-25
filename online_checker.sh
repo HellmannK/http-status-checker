93c24ab2a5d72270bfd2ef07ed3452f3291b65ed
Karim <karim.ellmann@posteo.de>
2023-12-25 23:32:13 +0100

    API fixes

--- online_checker.sh
+++ online_checker.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+# Requirements: mailutils, curl
+
 # Check if the script is running as root, if not, request root access
 if [ "$EUID" -ne 0 ]; then
   echo "Requesting root access..."
@@ -20,10 +22,21 @@ for ((i=1;i<=WEBSITE_COUNT;i++)); do
 
   # Check if website is not reachable
   if [ $HTTP_STATUS -ne 200 ]; then
-    # Stop and start the VM and send email
-    curl -s -X POST -H "Authorization: PVEAPIToken=$API_TOKEN" -H "Content-Type: application/json" -d '{}' "${API_URL}/nodes/${NODE[i]}/qemu/${VMID[i]}/status/stop"
+    # Determine the type of the resource (lxc or qemu)
+    if [[ ${LXC[i]} == "YES" ]]; then
+        TYPE="lxc"
+    elif [[ ${VM[i]} == "YES" ]]; then
+        TYPE="qemu"
+    else
+        echo "Neither LXC nor VM is set to YES for index $i"
+        continue
+    fi
+
+    # Stop and start the VM or LXC and send email
+    curl -s -X POST -H "Authorization: PVEAPIToken=$API_TOKEN" -H "Content-Type: application/json" -d '{}' "${API_URL}/nodes/${NODE[i]}/${TYPE}/${VMID[i]}/status/stop"
     sleep 5
-    curl -s -X POST -H "Authorization: PVEAPIToken=$API_TOKEN" -H "Content-Type: application/json" -d '{}' "${API_URL}/nodes/${NODE[i]}/qemu/${VMID[i]}/status/start"
+    curl -s -X POST -H "Authorization: PVEAPIToken=$API_TOKEN" -H "Content-Type: application/json" -d '{}' "${API_URL}/nodes/${NODE[i]}/${TYPE}/${VMID[i]}/status/start"
+    # Send Email if machine had http error code
     echo "VM ${VMID[i]} on node ${NODE[i]} has been restarted due to HTTP error $HTTP_STATUS on website ${URL[i]}" | mail -s "VM Restarted" $EMAIL
   fi
 done
