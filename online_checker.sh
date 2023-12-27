54f5d228f5c0dc92ec6130944983ba0d02d06ea0
Karim <karim.ellmann@posteo.de>
2023-12-27 16:08:37 +0100

    Updated API calls

--- online_checker.sh
+++ online_checker.sh
@@ -14,23 +14,25 @@ source $HTTP_FILE
 SENT_TO_EMAIL="your-email@domain.com"
 FROM_EMAIL="from@domain.com"
 
-  # Check if HTTP status code is in the list of codes to watch
-  if [[ " ${HTTP_CODES[*]} " == *"$HTTP_STATUS"* ]]; then
-    # Determine the type of the resource (lxc or qemu)
-    if [[ ${LXC[i]} == "YES" ]]; then
-        TYPE="lxc"
-    elif [[ ${VM[i]} == "YES" ]]; then
-        TYPE="qemu"
-    else
-        echo "Neither LXC nor VM is set to YES for index $i"
-        continue
-    fi
+for ((i=0;i<${#HTTP_CODES[@]};i++)); do
+   HTTP_STATUS=$(curl -o /dev/null -s -w "%{http_code}\n" "${URL[i]}")    
+    # Check if HTTP status code is in the list of codes to watch
+    for ((j=0;j<${#HTTP_CODES[@]};j++)); do
+        if [[ "${HTTP_CODES[j]}" == "$HTTP_STATUS" ]]; then
+            # Determine the type of the resource (lxc or qemu)
+            if [[ ${LXC[i]} == "YES" ]]; then
+                TYPE="lxc"
+            elif [[ ${VM[i]} == "YES" ]]; then
+                TYPE="qemu"
+            else
+                echo "Neither LXC nor VM is set to YES for index $i"
+                exit 1
+            fi
 
-    # Stop and start the VM or LXC and send email
-    curl -s -X POST -H "Authorization: PVEAPIToken=$API_TOKEN" -H "Content-Type: application/json" -d '{}' "${API_URL}/nodes/${NODE[i]}/${TYPE}/${VMID[i]}/status/stop"
-    sleep 5
-    curl -s -X POST -H "Authorization: PVEAPIToken=$API_TOKEN" -H "Content-Type: application/json" -d '{}' "${API_URL}/nodes/${NODE[i]}/${TYPE}/${VMID[i]}/status/start"
+    # Reboot the VM or LXC and send email
+    curl -s -X POST -H "Authorization: PVEAPIToken=$API_TOKEN" -H "Content-Type: application/json" -d '{}' "${API_URL}/nodes/${NODE[i]}/${TYPE}/${VMID[i]}/status/reboot"
     # Send Email if machine had http error code
-    echo "VM ${VMID[i]} on node ${NODE[i]} has been restarted due to HTTP error $HTTP_STATUS on website ${URL[i]}" | mail -r "$FROM_EMAIL" -s "VM Restarted" "$SENT_TO_EMAIL"
-  fi
+    echo "VM ${VMID[i]} on node ${NODE[i]} has been restarted due to HTTP error $HTTP_STATUS on website ${URL[i]}" | mail -r "$FROM_EMAIL" -s "VM Restarted" "$SENT_TO_EMAIL"  
+       fi
+    done
 done
